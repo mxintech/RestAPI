2fb64ec99d9028e67ce1e5bebc8d84fffce85db3
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-11-09 11:56:59 -0600

    User handle func

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	log.Fatal()
+	http.HandleFunc("/user", User)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func User(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Get users"))
+	}
 }

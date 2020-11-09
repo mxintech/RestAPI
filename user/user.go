b3f201fe39d5632d58655519ca11fbe12b0faa3f
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-11-09 12:47:47 -0600

    user storage as a map

--- user/user.go
+++ user/user.go
@@ -1,6 +1,38 @@
 package user
 
-import "net/http"
+import (
+	"net/http"
+)
+
+var storage Storage
+
+func init() {
+	storage = make(map[string]*User)
+}
+
+type Storage map[string]*User
+
+// User...
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func (U *Storage) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (U *Storage) DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (U *Storage) UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (U Storage) GetUser(w http.ResponseWriter, r *http.Request) {
+
+}
 
 // HandleUser handle method for request /user
 func HandleUser(w http.ResponseWriter, r *http.Request) {
@@ -8,5 +40,14 @@ func HandleUser(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Want's to request all users? :)"))
+	case "POST":
+
+	case "PUT":
+
+	case "DELETE":
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 }
